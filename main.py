72e7c53fe272ab0f3fa9d952f436771790774108
Abla Dawood <72917429+abladawood@users.noreply.github.com>
2022-07-14 21:43:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import os
-list1 = [4,3,5,3,2,5]
-list1.reverse()
-print("the reversed list is", list1)
-
-list2 = [3,6,5,4,8,7,1]
-list2.sort()
-print("the sorted list is:", list2)
+for path, subdirs, files in os.walk('/Users/ablosh/Desktop/Semester B'):
+    for name in files:
+        print(os.path.join(path, name))
 
 
