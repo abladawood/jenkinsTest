b22f60237265677a00b7877e7aeda10112b9fe91
Abla Dawood <72917429+abladawood@users.noreply.github.com>
2022-07-21 16:06:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 print(sys.argv[1])
-filePath = '/Users/ablosh/Desktop/Semester B'
+filePath = sys.argv[1]
 for path,subdir,files in os.walk(filePath):
     for file in files:
         for subfile in file:
